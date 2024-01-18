2ee1b99a60d82a0438ec675371ef70e5631d0674
Oskar Munz <oskar.munz@gmx.de>
2024-01-19 00:26:26 +0100

    add format on save for diamond lsp

--- after/ftplugin/diamond.lua
+++ after/ftplugin/diamond.lua
@@ -1,4 +1,18 @@
+function diamond_fmt(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
+	end
+end
+
 vim.lsp.start({
 	cmd = { "/home/oskar/programs/diamond-c/bin/diamond", "--lsp" },
 	root_dir = vim.fn.getcwd(),
+	on_attach = diamond_fmt,
 })
