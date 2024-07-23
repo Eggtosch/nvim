b01543186ed742add7df1f05094e2688a79d1e70
Oskar Munz <omunz@jusst.de>
2024-07-23 11:22:28 +0200

    gitsigns: remove deprecated options

--- config/gitsigns.lua
+++ config/gitsigns.lua
@@ -1,7 +1,7 @@
 require("gitsigns").setup {
 	signs = {
-		add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr" },
-		change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr" },
-		delete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr" },
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "-" },
 	},
 }
